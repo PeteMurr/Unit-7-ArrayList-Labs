bffa19eae924c10a2faee94b0c62a0344b6f8840
svolp <143809611+svolp@users.noreply.github.com>
2025-02-04 09:39:19 -0500

    Add files via upload

---
+++ LastRunner.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+//add imports as needed
+
+public class LastRunner
+{
+	public static void main( String args[] )
+	{
+	}
+}
---
+++ ListOddToEven.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.ArrayList;
+
+public class ListOddToEven
+{
+	public static int go( ArrayList<Integer> ray )
+	{
+		return false;
+	}
+}
